449c93b69fcdc0092dc5a040bd28fe7ebf471ec4
TonyStark0801 <mishrashubham2001@gmail.com>
2024-01-25 14:07:57 +0530

    Old Otp service removed

--- src/otpVerification/otpVerification.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OtpVerificationService } from './otpVerification.service';
-import { otpVerificationController } from './otpVerification.controller';
-
-@Module({
-  providers: [OtpVerificationService],
-  controllers: [otpVerificationController]
-})
-export class OtpVerificationModule {}
